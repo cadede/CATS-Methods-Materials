a8da31e1ae64f06da44549789c58ee02253e080d
cadede <59025499+cadede@users.noreply.github.com>
2020-12-04 09:13:36 -0800

    reorganization

---
+++ CATSMatlabTools/renameVids.m
@@ -0,0 +1,33 @@
+% rename video files
+[filename,fileloc] = uigetfile('*.mp4','Grab files to rename','multiselect','on');
+if ~iscell(filename); filename = {filename}; end
+d = dir(fileloc); D = {d.name};
+prh = ~cellfun(@isempty,cellfun(@(x) regexp(x,'prh.mat$'),D,'uniformoutput',false));
+for i = 1:length(filename)
+    w = ~cellfun(@isempty,cellfun(@(x) regexp(x,['^' getWhaleID(filename{i})]),D,'uniformoutput',false));
+    if any(w&prh); vidT = true; oi = D(prh&w); load([fileloc oi{1}],'vidDN','vidDurs','INFO'); else vidT = false; end
+    I = strfind(filename{i},' (Stitched Clip)');
+    vn = min(strfind(filename{i}, '('));
+    dI = [min(strfind(filename{i},')')) max(strfind(filename{i},'-'))]; vn2 = max([dI(2) vn]); dI = dI(dI>vn);
+    dI2 = dI(1); 
+    dI = min(dI);
+    vn = str2num(filename{i}(vn+1:dI-1));
+    vn2 = str2num(filename{i}(vn2+1:dI2-1));
+    if ~isempty(I); f = [filename{i}(1:I-1) '.mp4']; else f = filename{i}; end
+    if vidT;
+        if  i==1 ; v = mmread([fileloc filename{i}],[1 10]); dur = v.totalDuration;
+            if dur < vidDurs(vn); vidDN(vn) = vidDN(vn) + (vidDurs(vn)-dur)/24/60/60; end
+        end
+        f = [f(1:end-4) ' (' datestr(vidDN(vn),'mm.dd HHMMSS') ').mp4'];
+    end
+    movefile([fileloc filename{i}],[fileloc f]);
+end
+
+disp(['All files renamed for ' INFO.whaleName]);
+    
+% Rename in a LOOP
+% for n = 1:numel(names)
+%     oldname = [d names{n}];
+%     newname = sprintf('%s%0*s',d,mLen, names{n});
+%     dos(['rename "' oldname '" "' newname '"']); % (1)
+% end
